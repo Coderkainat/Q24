788d91491721f709347101402e9427fca9789c49
Coderkainat <159674299+Coderkainat@users.noreply.github.com>
2024-03-14 05:40:49 +0500

    Add files via upload

---
+++ Q24/Q24.js
@@ -0,0 +1,30 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array
+// const string1: string = "Apple";
+// const string2: string = "Mango";
+// console.log(string1===string2); 
+// console.log(string1 !== string2);
+// • Tests using the lower case function
+// const mixedCaseString: string = "Hello world";
+// console.log(mixedCaseString.toLowerCase() === "helloworld")
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// const num1: number = 20;
+// const num2:number = 5;
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 <= num2);
+//  Tests using "and" and "or" operators
+// const Condition1: boolean = true
+// const condition2: boolean = false;
+// console.log(Condition1 && condition2);
+// console.log(Condition1 || condition2)
+// Test whether an item is in a array
+var Fruits = ["Mango", "Apple", "peach", "Grapes"];
+console.log(Fruits.includes("apple"));
+console.log(Fruits.includes("Apple"));
